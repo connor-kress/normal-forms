a549f8bad6479824d2024a088daee74072cef5bc
Connor Kress <ckress@ufl.edu>
2025-04-12 23:36:02 -0400

    Primary key functionality and test data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Self
+from typing import Optional, Self
+from pprint import pprint
 
 
 def contains_all[T](__a: set[T], __b: set[T]) -> bool:
@@ -33,3 +34,45 @@ class Relation:
 
     def contains_any(self, attrs: set[str]) -> bool:
         return contains_any(self.attrs, attrs)
+
+
+def get_primary_key(relation: Relation, deps: list[FD]) -> set[str]:
+    key = relation.attrs.copy()
+    for dep in deps:
+        # print(f"\tDep: {dep}")
+        if relation.contains_all(dep.lhs):
+            # print("\tcontains")
+            key -= dep.rhs
+        else:
+            # print("\tnot contains")
+            pass
+    return key
+
+
+def main() -> None:
+    # relation = Relation({"A", "B", "C", "D"})
+    relation = Relation({
+        "traveler ssn", "agent", "years experience",
+         "trip id", "start location", "end location",
+         "passport number", "expiration date",
+    })
+    deps = [
+        # FD({"A"}, {"C"}),
+        # FD({"C"}, {"D"}),
+        # FD({"A", "C"}, {"D"}),
+        FD({"agent"}, {"years experience"}),
+        FD({"traveler ssn"}, {"passport number"}),
+        FD({"passport number"}, {"expiration date"}),
+        FD({"trip id"}, {"start location", "end location"}),
+    ]
+    print("Relation:")
+    pprint(relation)
+    print("\nDependencies:")
+    for dep in deps:
+        print(dep)
+    print()
+    print(get_primary_key(relation, deps))
+
+
+if __name__ == "__main__":
+    main()

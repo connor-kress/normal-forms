bd449b4d0de268c5ca7e0e59207d899eb62a46cc
Connor Kress <ckress@ufl.edu>
2025-04-12 23:38:31 -0400

    Draft dependency violation calculation

--- main.py
+++ main.py
@@ -49,6 +49,53 @@ def get_primary_key(relation: Relation, deps: list[FD]) -> set[str]:
     return key
 
 
+def get_dependency_violation(
+    relation: Relation,
+    deps: list[FD],
+    key: set[str],
+) -> Optional[FD]:
+    transitive_dep: Optional[FD] = None
+    for dep in deps:
+        if contains_all(dep.lhs, key):
+            print("Valid dependency (superkey lhs):", dep)
+        elif contains_all(key, dep.lhs):
+            print("Partial dependency:", dep)
+            if not relation.contains_any(dep.rhs):
+                # Maybe we have to check for this later, after this function too
+                print("What?")
+                input("pause")
+            return dep.copy()
+        else:
+            print("Transitive dependency:", dep)
+            if transitive_dep is None:
+                transitive_dep = dep.copy()
+    return transitive_dep
+
+
+def bcnf_decomposition(
+    relations: list[Relation],
+    deps: list[FD],
+) -> list[Relation]:
+    while True:
+        print("Relations: ")
+        pprint(relations)
+        violation_found = False
+        for relation in relations:
+            key = get_primary_key(relation, deps)
+            print("Key:", key)
+            violation = get_dependency_violation(relation, deps, key)
+            if violation is None:
+                continue
+            else:
+                violation_found = True
+            print("Violation:", violation)
+            input("Pause")
+            break
+        if not violation_found:
+            break
+    return [Relation(set())]
+
+
 def main() -> None:
     # relation = Relation({"A", "B", "C", "D"})
     relation = Relation({
@@ -71,7 +118,7 @@ def main() -> None:
     for dep in deps:
         print(dep)
     print()
-    print(get_primary_key(relation, deps))
+    new_relations = bcnf_decomposition([relation], deps)
 
 
 if __name__ == "__main__":
